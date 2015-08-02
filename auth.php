4ecce940d0ffacc6ead07f4a09bdc2d9593a1d91
Nesseref <orc@frogbox.es>
2015-08-01 17:02:20 -0700

    moved to mysqli

--- auth.php
+++ auth.php
@@ -5,26 +5,26 @@ $username = "";
 $password = "";
 $dbname = "";
 
-$conn = mysql_connect($host, $username, $password);
-if (!$conn) {
-        die('Could not connect: ' . mysql_error());
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_errno) {
+        die('Could not connect: ' . $conn->connect_error);
 }
 
 if(empty($_GET['name']) || empty($_GET['swfurl']))
 {
         http_response_code(400);
-        die("Bad query");
+        die("Bad query 1");
 }
 else
 {
-        $name = mysql_real_escape_string($_GET['name']);
+        $name = mysqli_real_escape_string($conn, $_GET['name']);
         preg_match($pattern, $_GET['swfurl'], $matches);
 }
 
-mysql_select_db($dbname);
+#mysql_select_db($dbname);
 $query = "SELECT idhash FROM users WHERE username = '$name'";
-$result = mysql_query($query);
-$row = mysql_fetch_assoc($result);
+$result = $conn->query($query);
+$row = $result->fetch_array(MYSQLI_ASSOC);
 
 if ($matches[1] == $row['idhash']){
         http_response_code(200);
@@ -32,7 +32,6 @@ if ($matches[1] == $row['idhash']){
 }
 else{
         http_response_code(400);
-        die("Bad query");
+        die("Bad query 2");
 }
 ?>
-
